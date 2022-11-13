e639442358230c65f1cc650d70c099a25c620079
TomerBenJonathan <117282271+TomerBenJonathan@users.noreply.github.com>
2022-11-14 00:06:41 +0200

    test

---
+++ main.js
@@ -0,0 +1,18 @@
+
+const appDiv = document.getElementById("app");
+appDiv.innerHTML= '<h1>JS tarter</h1>';
+console.log('hello world');
+
+const person= {
+    name: 'Tomer',
+    surname: 'Ben-jonathan'
+
+}
+
+console.log(person.surname)
+
+let x=5;
+
+
+
+document.write('<h1>ttt</h1');
